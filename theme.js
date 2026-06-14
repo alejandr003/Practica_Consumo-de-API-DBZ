c8ba3474994ec8ca6655e81f3da70a6bb9fc9a56
alejandr003 <alejandrochan147@hotmail.com>
2026-06-14 17:20:13 -0500

    feat: update theme with DBZ colors and Reem Kufi font import

--- theme.js
+++ theme.js
@@ -1,20 +1,31 @@
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
 
-// Create a theme instance.
 const theme = createTheme({
-  cssVariables: true,
-  palette: {
-    primary: {
-      main: '#556cd6',
+    cssVariables: true,
+    palette: {
+        primary: {
+            main: '#fe9e0d',
+        },
+        secondary: {
+            main: '#2A2E35',
+        },
+        error: {
+            main: '#f44336',
+        },
+        background: {
+            default: '#f6f6f6',
+        },
     },
-    secondary: {
-      main: '#19857b',
+    typography: {
+        fontFamily: '"Reem Kufi", "Roboto", "Helvetica", "Arial", sans-serif',
     },
-    error: {
-      main: red.A400,
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: {
+                '@import': 'url(https://fonts.googleapis.com/css2?family=Reem+Kufi:wght@400;500;600;700&display=swap)',
+            },
+        },
     },
-  },
 });
 
 export default theme;
